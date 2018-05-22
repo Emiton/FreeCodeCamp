391f84e88af74df40ac3c4996849ac7ef3ee61fe
Emiton <emiton_alves@my.uri.edu>
2018-05-22 17:23:31 +0000

    added solution for convert html entities

---
+++ intermediate-algorithm-scripting/convert_html_entities.js
@@ -0,0 +1,31 @@
+// This function will replace certain characters with thier HTML counterparts
+function convertHTML(str) {
+//My original one line solution
+//   str = str.replace(/&/g, '&amp;')
+//   .replace(/>/g, '&gt;')
+//   .replace(/"/g, '&quot;')
+//   .replace(/'/g, '&apos;')
+//   .replace(/</g, '&lt;');
+
+// Solution using hash map. Challenge issued by Cliff
+  var basicHashMap = {
+    ">" : "&gt;",
+    '"' : "&quot;",
+    "'" : "&apos;",
+    "<" : "&lt;",
+    "&" : "&amp;"
+  };
+  
+  var checkStr = str.split('');
+  // Go through each word and check if it matches a value in the hash map
+  // Replace with HTML value if it is found in the hash map
+  var replaceStr = checkStr.map(function (word){
+    if (basicHashMap.hasOwnProperty(word)) {
+      word = basicHashMap[word];
+    }
+    return word;
+  });
+
+  var fixedStr = replaceStr.join('');
+  return fixedStr;
+}
