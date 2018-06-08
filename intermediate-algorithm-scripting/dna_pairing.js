47149b13158125e40f1a7b4b376c6b19481f564c
Emiton <emiton_alves@my.uri.edu>
2018-06-08 20:08:33 +0000

    fixed spelling errors in the comments

--- intermediate-algorithm-scripting/dna_pairing.js
+++ intermediate-algorithm-scripting/dna_pairing.js
@@ -2,7 +2,7 @@
 * This function accepts a string in which each letter represents a strand of DNA
 * The DNA strands are matched with a corresponding strand
 * The given strand and corresposning strand are stored in a 'pair array' of size 2
-* Coresponding pairs can be found in the hash map
+* Corresponding pairs can be found in the hash map
 * Return an aray holding all of the pair arrays
 */
 
@@ -19,7 +19,7 @@ function pairElement(str) {
 
   // For every strand in given string, create an array of size 2
   // Insert given DNA strand in first spot
-  // Insert mathing DNA strand in second spot using 'DNApairs" hash map 
+  // Insert matching DNA strand in second spot using 'DNApairs" hash map 
   for (var i = 0; i < str.length; i++) {
     pairArray[i] = [ str[i] , DNApairs[ str[i] ] ];
   }
