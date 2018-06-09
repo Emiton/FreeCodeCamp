b33b1d0196e37842d40fda7a15fa90f7f4263efc
Emiton <emiton_alves@my.uri.edu>
2018-06-09 23:31:42 +0000

    added description for wherefore art thou

---
+++ intermediate-algorithm-scripting/wherefore_art_thou.js
@@ -0,0 +1,5 @@
+/**
+* This function will accept 2 arguments. The first being an array of objects known as the collection.
+* The second argument is an object known as the source. The function returns an array of objects. Any 
+* object in the collection that is equivalent to the source object is added to the return array.
+**/  
