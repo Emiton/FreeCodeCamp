e65e7eab7cd336476a426ad361233a2c5dc0f4bd
Emiton <emiton_alves@my.uri.edu>
2018-06-10 00:08:23 +0000

    updated resources for later on

--- intermediate-algorithm-scripting/wherefore_art_thou.js
+++ intermediate-algorithm-scripting/wherefore_art_thou.js
@@ -2,4 +2,8 @@
 * This function will accept 2 arguments. The first being an array of objects known as the collection.
 * The second argument is an object known as the source. The function returns an array of objects. Any 
 * object in the collection that is equivalent to the source object is added to the return array.
+*
+* Resources:
+* 1. http://adripofjavascript.com/blog/drips/filtering-arrays-with-array-filter.html
+* 2.  
 **/  
